fd6fa9fb5947c5333e7334834cbce52142681f52
leidenglai <819441338@qq.com>
2020-04-30 16:49:01 +0800

    多语言模块更新

---
+++ src/locales/index.ts
@@ -0,0 +1,4 @@
+import appIntlEn from './en-US'
+import appIntlZh from './zh-CN'
+
+export { appIntlEn, appIntlZh }
--- src/locales/zh-CN/index.ts
+++ src/locales/zh-CN/index.ts
@@ -1,13 +1,13 @@
 // 引入组件多语言
 import messages from './messages.json'
 // antd
-import antdLocale from 'antd/es/locale/en_US'
+import antdLocale from 'antd/es/locale/zh_CN'
 
 const appLocale = {
   // 合并所有 messages, 加入组件的 messages
   messages: Object.assign({}, messages),
   // locale
-  locale: 'en-US',
+  locale: 'zh-CN',
   antdLocale
 }
 
